ba8fa6774db1f16b8b09a155976e4faa612ae561
mu0363 <dde036@gmail.com>
2022-08-16 11:28:02 +0900

    AppShellを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import "../style.css";
 import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+import { PlayerLayout } from "../layout/PlayerLayout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: "light",
+      }}
+    >
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </MantineProvider>
+  );
 }
 
 export default MyApp;
