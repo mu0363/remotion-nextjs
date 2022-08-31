84d29e0e3a34597e8e473d99d77cc694ae9455d2
mu0363 <dde036@gmail.com>
2022-09-01 07:33:56 +0900

    PlayButtonをコンポーネント化

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -4,7 +4,7 @@ import { ActionIcon, Box } from "@mantine/core";
 import { Player as RemotionPlayer, PlayerRef } from "@remotion/player";
 import { IconPlayerPlay, IconPlayerPause } from "@tabler/icons";
 import { useAtomValue } from "jotai";
-import { useEffect, useRef, useState } from "react";
+import { FC, RefObject, useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import type { CustomNextPage } from "next";
 import { Form } from "src/components/Form";
@@ -12,23 +12,15 @@ import { TimelineCard } from "src/components/TimelineCard";
 import { EditLayout } from "src/layout/EditLayout";
 import { currentFrameAtom } from "src/libs/atom";
 import { TEMPLATE1_DURATION, timelineScenes } from "src/libs/const/remotion-config";
-import { selectAllActiveScene } from "src/libs/store/features/activeSceneSlice";
+import { ActiveSceneSlice, selectAllActiveScene } from "src/libs/store/features/activeSceneSlice";
 import { selectAllTemplate1Data } from "src/libs/store/features/template1Slice";
 import { Template1 } from "src/remotion/Template1";
 
 const Player: CustomNextPage = () => {
-  const currentFrame = useAtomValue(currentFrameAtom);
   const [isPlaying, setIsPlaying] = useState(false);
   const template1Data = useSelector(selectAllTemplate1Data);
   const activeSceneData = useSelector(selectAllActiveScene);
   const playerRef = useRef<PlayerRef>(null);
-
-  const calculateTime = (fps: number) => {
-    const minute = Math.floor(fps / (30 * 60));
-    const second = Math.floor(fps / 30);
-    const padSecond = String(second).padStart(2, "0");
-    return `${minute}:${padSecond}`;
-  };
 
   useEffect(() => {
     if (playerRef.current) {
@@ -57,23 +49,12 @@ const Player: CustomNextPage = () => {
       </div>
       <div className="mx-0 md:mx-5">
         <div className="flex items-center">
-          <div className="flex flex-col items-center">
-            <ActionIcon
-              color="red"
-              size="lg"
-              radius="xl"
-              variant="filled"
-              className="mx-5 shadow-md"
-              onClick={() => {
-                playerRef.current?.toggle();
-                setIsPlaying(!isPlaying);
-              }}
-            >
-              {isPlaying ? <IconPlayerPause size={18} /> : <IconPlayerPlay size={18} />}
-            </ActionIcon>
-            <p className="mx-5 mt-2 text-xs font-bold text-gray-600">{calculateTime(currentFrame)}</p>
-            {/* <p className="mx-5 mt-2 text-xs font-bold text-gray-600">02:30</p> */}
-          </div>
+          <PlayButton
+            playerRef={playerRef}
+            isPlaying={isPlaying}
+            setIsPlaying={setIsPlaying}
+            activeSceneData={activeSceneData}
+          />
           <Box sx={{ display: "flex", overflowX: "scroll" }}>
             {timelineScenes.map((card) => (
               <div key={card.id}>
@@ -94,3 +75,47 @@ const Player: CustomNextPage = () => {
 Player.getLayout = EditLayout;
 
 export default Player;
+
+type PlayButtonProps = {
+  playerRef: RefObject<PlayerRef>;
+  isPlaying: boolean;
+  setIsPlaying: (isPlaying: boolean) => void;
+  activeSceneData: ActiveSceneSlice;
+};
+
+const PlayButton: FC<PlayButtonProps> = ({ playerRef, isPlaying, setIsPlaying, activeSceneData }) => {
+  const currentFrame = useAtomValue(currentFrameAtom);
+
+  useEffect(() => {
+    if (playerRef.current) {
+      playerRef.current.pause();
+      playerRef.current.seekTo(activeSceneData.from);
+    }
+  }, [activeSceneData, playerRef]);
+
+  const calculateTime = (fps: number) => {
+    const minute = Math.floor(fps / (30 * 60));
+    const second = Math.floor(fps / 30);
+    const padSecond = String(second).padStart(2, "0");
+    return `${minute}:${padSecond}`;
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <ActionIcon
+        color="red"
+        size="lg"
+        radius="xl"
+        variant="filled"
+        className="mx-5 shadow-md"
+        onClick={() => {
+          playerRef.current?.toggle();
+          setIsPlaying(!isPlaying);
+        }}
+      >
+        {isPlaying ? <IconPlayerPause size={18} /> : <IconPlayerPlay size={18} />}
+      </ActionIcon>
+      <p className="mx-5 mt-2 text-xs font-bold text-gray-600">{calculateTime(currentFrame)}</p>
+    </div>
+  );
+};
