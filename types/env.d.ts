f0df0fbd359c47ca59f681da06914223723fef51
mu0363 <dde036@gmail.com>
2022-09-08 10:18:13 +0900

    環境変数のtypeを設定

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,5 +1,5 @@
 declare namespace NodeJS {
-  type ProcessEnv = {
+  interface ProcessEnv  {
     readonly NODE_ENV: "development" | "production" | "test";
     readonly REMOTION_AWS_ACCESS_KEY_ID: string;
     readonly REMOTION_AWS_SECRET_ACCESS_KEY: string;
