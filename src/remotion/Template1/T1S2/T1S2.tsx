edf3cd7f08a439108cf2a81d53db60a287fb694e
mu0363 <dde036@gmail.com>
2022-09-09 11:15:53 +0900

    その他のimage_urlにも設定

--- src/remotion/Template1/T1S2/T1S2.tsx
+++ src/remotion/Template1/T1S2/T1S2.tsx
@@ -22,7 +22,7 @@ export const T1S2: FC<Props> = ({ sceneState }) => {
           justifyContent: "center",
         }}
       >
-        <T1S2_Image1 image={image_url} />
+        {image_url !== undefined && <T1S2_Image1 image={image_url} />}
         <T1S2_Text1 text={text} />
         <Subtitle />
       </AbsoluteFill>
